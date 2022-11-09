efed00165059c767842b2b2b28b92f2d05fa67f8
IlyaLeontev <neerex101@yandex.ru>
2022-11-09 11:29:17 +0300

    Sphere Generation With Resolution

---
+++ Assets/Scripts/TerrainFace.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class TerrainFace
+{
+    private Mesh _mesh;
+    private int _resolution;
+    private Vector3 _localUp;
+
+    private Vector3 _axisA;
+    private Vector3 _axisB;
+
+    public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
+    {
+        _mesh = mesh;
+        _resolution = resolution;
+        _localUp = localUp;
+
+        _axisA = new Vector3(localUp.y, localUp.z, localUp.x);
+        _axisB = Vector3.Cross(localUp, _axisA);
+    }
+
+    public void ConstructMesh()
+    {
+        Vector3[] vertices = new Vector3[_resolution * _resolution];
+        int triangleVerticesCount = (_resolution - 1) * (_resolution - 1) * 6;
+        int[] triangles = new int[triangleVerticesCount];
+        int triangleIndex = 0;
+        
+        for (int y = 0; y < _resolution; y++)
+        {
+            for (int x = 0; x < _resolution; x++)
+            {
+                int i = x + y * _resolution;
+                Vector2 percent = new Vector2(x, y) / (_resolution - 1);
+                Vector3 pointOnUnitCube = _localUp + (percent.x - 0.5f) * 2 * _axisA + (percent.y - 0.5f) * 2 * _axisB;
+                Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
+                vertices[i] = pointOnUnitSphere;
+
+                if (x != _resolution - 1 && y != _resolution - 1)
+                {
+                    triangles[triangleIndex + 0] = i;
+                    triangles[triangleIndex + 1] = i + _resolution + 1;
+                    triangles[triangleIndex + 2] = i + _resolution;
+                    
+                    triangles[triangleIndex + 3] = i;
+                    triangles[triangleIndex + 4] = i + 1;
+                    triangles[triangleIndex + 5] = i + _resolution + 1;
+
+                    triangleIndex += 6;
+                }
+            }
+        }
+        
+        _mesh.Clear();
+        _mesh.vertices = vertices;
+        _mesh.triangles = triangles;
+        _mesh.RecalculateNormals();
+    }
+}
